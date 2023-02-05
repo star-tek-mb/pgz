e00ba088c2bca703a367585110bf3b9a05594f0c
star-tek-mb <noob.tek.mb@gmail.com>
2023-02-05 14:56:47 +0500

    update to zig master

--- build.zig
+++ build.zig
@@ -1,13 +1,23 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
-    const mode = b.standardReleaseOptions();
-    const lib = b.addStaticLibrary("pgz", "src/pgz.zig");
-    lib.setBuildMode(mode);
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const lib = b.addStaticLibrary(.{
+        .name = "pgz",
+        .root_source_file = .{ .path = "src/pgz.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
     lib.install();
 
-    const main_tests = b.addTest("src/pgz.zig");
-    main_tests.setBuildMode(mode);
+    const main_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/pgz.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
 }
