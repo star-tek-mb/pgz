28b1b5da05c7543b78604bb63bbb99363e4d91d4
star-tek-mb <noob.tek.mb@gmail.com>
2023-04-28 22:40:48 +0500

    update to latest zig 0.11.0-dev.2868+1a455b2dd

--- build.zig
+++ build.zig
@@ -14,7 +14,7 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-    lib.install();
+    b.installArtifact(lib);
 
     const main_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/pgz.zig" },
@@ -23,6 +23,7 @@ pub fn build(b: *std.Build) void {
     });
     main_tests.emit_docs = .emit;
 
+    const run_main_tests = b.addRunArtifact(main_tests);
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.run().step);
+    test_step.dependOn(&run_main_tests.step);
 }
